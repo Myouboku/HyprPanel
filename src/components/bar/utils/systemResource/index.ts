a69a7dd52902e8f3d4f6b2c5b38b1011a96d8dc2
Myouboku <71690611+Myouboku@users.noreply.github.com>
2025-11-04 21:19:48 +0100

    fix: round ram value

--- src/components/bar/utils/systemResource/index.ts
+++ src/components/bar/utils/systemResource/index.ts
@@ -64,7 +64,7 @@ export const renderResourceLabel = (
         return SizeConverter.fromBytes(free).formatAuto(precision);
     }
 
-    return `${percentage}%`;
+    return `${percentage.toFixed(precision)}%`;
 };
 
 /**
